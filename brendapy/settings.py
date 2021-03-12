16cf633a88112a62c389251841e640277bfaf467
Djomangan Adama OUATTARA <douattara@gencovery.com>
2021-03-12 13:30:56 +0000

    First commit after reconstruction of the architecture

---
+++ brendapy/settings.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Paths to main resources and resource management.
+
+Due to the size limits of git and pypi the large resources
+cannot be managed/included in git and pypi.
+These resources have to be loaded from online resources on
+first import.
+
+"""
+import os
+import logging
+from zipfile import ZipFile
+
+from gws.settings import Settings
+
+import requests
+import shutil
+
+settings = Settings.retrieve()
+
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+RESOURCES_PATH = settings.get_dir("biota:biodata_dir")
+BRENDA_FILE = os.path.join(RESOURCES_PATH, "brenda", "brenda", "brenda_download.txt")
+TAXONOMY_DATA = os.path.join(RESOURCES_PATH, "brenda", "taxonomy", "taxonomy.json")
+TAXONOMY_ZIP = os.path.join(RESOURCES_PATH, "brenda", "taxonomy", "taxdump.zip")
+BTO_DATA = os.path.join(RESOURCES_PATH, "brenda", "bto", "bto.json")
+CHEBI_DATA = os.path.join(RESOURCES_PATH, "brenda", "chebi", "chebi.json")
